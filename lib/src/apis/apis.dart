e6c917735a274ee6d761a06e12a5a4b7ab8e2f9c
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-06-24 10:51:34 +0600

    Initial commit

---
+++ lib/src/apis/apis.dart
@@ -0,0 +1,5 @@
+String baseAPI = "https://newsapi.org/";
+String articleApple =
+    "v2/everything?q=apple&from=2025-06-23&to=2025-06-23&sortBy=popularity";
+String articleTesla =
+    "v2/everything?q=tesla&from=2025-05-24&sortBy=publishedAt";
